2b66460aec79e4056405c22543fb42af4467cdf0
MasixoleMax <masixole.menqe@umuzi.org>
2020-01-30 15:07:24 +0200

    commiting all changes

---
+++ blog/views.py
@@ -0,0 +1,73 @@
+from django.shortcuts import render
+
+posts = [
+
+    {
+        'autor':'Masixole Menqe',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'August 27, 2018'
+    },
+    {
+        'autor':'Masixole Menqe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'August 28, 2018'
+    }
+]
+
+
+def home(request):
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
+
+def about(request):
+    return render(request, 'blog/about.html', {'title': 'About'})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
